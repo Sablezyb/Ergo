1716af18fd4ef8f2d86202ac2df194177aebf508
Roma <mig29.romakos@mail.ru>
2021-02-22 13:06:38 +0600

    created slider

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000
+    });
+  });
